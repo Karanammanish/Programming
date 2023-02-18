925eae36629ac13cd6f3ff2178ecf50630256f49
Karanammanish <103556008+Karanammanish@users.noreply.github.com>
2023-02-18 14:18:21 +0530

    Add files via upload

---
+++ banking_site/forgot_password.js
@@ -0,0 +1,12 @@
+function submit()
+{
+   let x = document.getElementsByName("Lost").values;
+   if (x === "ATM")
+   {
+        window.open("file:///C:/Users/manish%20reddy/Desktop/Front%20end/project/banking_site/atm.htm");
+   }
+   else
+   {
+        window.open("../banking_site/pp.htm");
+   }
+}   
